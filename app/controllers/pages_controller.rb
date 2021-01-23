a99e11bf4281d13b1b0e3ee5b6be264aa5cc6211
andre ferrer <andrerferrer@hotmail.com>
2021-01-23 06:17:54 -0300

    Finish the lecture

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
+  # this time, we want home to be allowed
+  # we whitelist it
+  skip_before_action :authenticate_user!, only: :home
   def home
+  end
+
+  def about
   end
 end
